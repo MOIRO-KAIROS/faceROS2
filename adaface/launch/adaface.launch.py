07d0c454f7ad4da8d9c185de6dfe6490a17c73a6
alsgk0404 <alsgk0404@hanyang.ac.kr>
2024-04-22 17:42:44 +0900

    save for merge

--- adaface/launch/adaface.launch.py
+++ adaface/launch/adaface.launch.py
@@ -4,12 +4,14 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument,IncludeLaunchDescription
 from launch.actions import LogInfo
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
 from launch.substitutions import LaunchConfiguration
 
-    
+from launch_ros.actions import Node
 def generate_launch_description():
-      # ARGS
+
+   #
+    # ARGS
     #
     model = LaunchConfiguration("fr_weight")
     model_cmd = DeclareLaunchArgument(
@@ -69,23 +71,18 @@ def generate_launch_description():
         choices=["0", "1", "2"],
         description="Specific reliability QoS of the input image topic (0=system default, 1=Reliable, 2=Best Effort)")
 
-    namespace_adaface = LaunchConfiguration("namespace_adaface")
-    namespace_adaface_cmd = DeclareLaunchArgument(
-        "namespace_adaface",
+    namespace = LaunchConfiguration("namespace")
+    namespace_cmd = DeclareLaunchArgument(
+        "namespace",
         default_value="adaface",
         description="Namespace for the nodes")
-    tracking_topic = LaunchConfiguration("detections")
-    tracking_topic_cmd = DeclareLaunchArgument(
-        "detections",
-        default_value="/yolo/tracking",
-        description="YOLOv8 tracking")
 
     # NODES
     adaface_node_cmd = Node(
-        package="adaface",
-        executable="face_recognition",
-        name="face_recongnition",
-        namespace=namespace_adaface,
+        package="yolov8_ros",
+        executable="adaface_node",
+        name="adaface_node",
+        namespace=namespace,
         parameters=[{
             "fr_weight": model,
             "device": device,
@@ -98,9 +95,8 @@ def generate_launch_description():
         }],
         remappings=[
             ("image_raw", input_image_topic),
-            ("detections", tracking_topic)
-        ],
-        output='screen',
+            ("detections", "tracking")
+        ]
     )
 
     ld = LaunchDescription()
@@ -114,28 +110,26 @@ def generate_launch_description():
     ld.add_action(video_cmd)
     ld.add_action(input_image_topic_cmd)
     ld.add_action(image_reliability_cmd)
-    ld.add_action(namespace_adaface_cmd)
-    ld.add_action(tracking_topic_cmd)
+    ld.add_action(namespace_cmd)
 
-    ld.add_action(IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                [get_package_share_directory('realsense2_camera'), '/launch/rs_launch.py']),
-                launch_arguments={
-                    'depth_module.profile':'848x480x30',
-                    'rgb_camera.profile':'640x480x30',
-                    'pointcloud.enable':'true'
-                }.items()
-        ))
-    ld.add_action( IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                [get_package_share_directory('yolov8_bringup'), '/launch/yolov8.launch.py']),
-                launch_arguments={
-                    'model':'yolov8m-pose.pt',
-                    'input_image_topic':'/camera/camera/color/image_raw',
-                    'target_frame':'camera_link'
-                }.items()
-        ))
-    
     ld.add_action(adaface_node_cmd)
-    
+
     return ld
+    # return LaunchDescription([
+        
+    #       # realsense package launch
+    #     IncludeLaunchDescription(
+    #         PythonLaunchDescriptionSource(
+    #             [get_package_share_directory('realsense2_camera'), '/launch/rs_launch.py']),
+    #     ),
+    #     IncludeLaunchDescription(
+    #         PythonLaunchDescriptionSource(
+    #             [get_package_share_directory('yolov8_bringup'), '/launch/yolov8.launch.py']),
+    #     ),
+    #     # input_image_topic,
+    #     # image_reliability,
+    #     ExecuteProcess(
+    #         cmd = ["ros2","run","adaface","face_recognition"], output="screen"
+    #     ), 
+    #     LogInfo(msg=['Execute launch files! (realsenseROS2 | YOLOv8 | Adaface)'])
+    # ])
