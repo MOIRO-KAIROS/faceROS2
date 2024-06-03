be22e7ee47b569a74a5feb950a40927130dffcde
yeonju52 <yeonju509@gmail.com>
2024-06-03 14:19:21 +0900

    edit main.py for store_embedding

--- adaface_ros/adaface_ros/script/main.py
+++ adaface_ros/adaface_ros/script/main.py
@@ -1,31 +1,41 @@
 import os
 import argparse
 import sys
-from adaface import run_video, store_embedding
 
-sys_path = os.path.dirname(__file__)
+sys_path = os.path.join(os.path.expanduser("~"), "moiro_ws/src/moiro_vision/adaface_ros/adaface_ros/script")
+
+from adaface  import AdaFace
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--mode', type=int, default=1, help='0: 임베딩 저장 1: webcam/video으로 run_video)')
+    parser.add_argument('--option', type=int, default=0, help='0: 임베딩 저장 1: webcam/video으로 run_video)')
     parser.add_argument('--video', type=str, default='0', help='0: webcam 또는 "video/iAM.mp4" 특정 비디오 path 경로')
-    parser.add_argument('--fr_weight', type=str, default='ir_50', help='face recognition weight')
+    parser.add_argument('--model', type=str, default='ir_50', help='face recognition weight')
     parser.add_argument('--thresh', nargs='+', type=str, default=.2, help='unknown confidence < .2')
     parser.add_argument('--max_obj', type=int, default=6, help='detect 가능한 최대 얼굴의 개수')
     parser.add_argument('--dataset', type=str, default='face_dataset/test', help='face dataset의 경로 (known face dataset)')
     
     opt = parser.parse_args()
     
-    if opt.mode == 0:
+    adaface = AdaFace(
+        model=opt.model,
+        option=opt.option,
+        dataset=opt.dataset,
+        video=opt.video,
+        max_obj=opt.max_obj,
+        thresh=opt.thresh,
+    )
+    
+    if opt.option == 0:
         opt.dataset = os.path.join(sys_path, opt.dataset)
-        store_embedding(opt)
-    elif opt.mode == 1:
+        adaface.store_embedding()
+    elif opt.option == 1:
         if (opt.video.isnumeric()):
             opt.video = int(opt.video)
-            run_video(opt)
+            adaface.run_video()
         else:
             opt.video = os.path.join(sys_path, opt.video)
-            run_video(opt)
+            adaface.run_video()
     else:
         print("Error: 잘못된 argument 입력")
         sys.exit(1)
